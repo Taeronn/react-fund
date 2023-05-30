192f506b5514fa7392a6820666a6c6875736fab9
Ilya Dragun <dragunilya18@gmail.com>
2023-05-31 00:31:33 +0300

    commit first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1>
+            Это приложение создано в рамках обуч. ролика.
+        </h1>
+    );
+};
+
+export default About;
